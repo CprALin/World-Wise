24fd274ab224c9fe926f0595397188a1e72657a3
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-05-03 22:04:34 +0300

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes , Route } from "react-router-dom";
+import Product from "./pages/product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+
 function App(){
 
    return(
-     <div>
-        WorldWise
-     </div>
+      <BrowserRouter>
+            <Routes>
+                 <Route path="/" element={<HomePage />} />
+                 <Route path="product" element={<Product />} />    
+                 <Route path="pricing" element={<Pricing />} />
+                 <Route path="*" element={<PageNotFound />} />
+            </Routes>
+      </BrowserRouter>
    )
 }
 
