e1a634597d09683425db42664bb290053a2e17a0
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-05-03 22:39:06 +0300

    -

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function HomePage(){    
     return(
         <div>
-            
+            <PageNav />
+            <h1>WorldWide</h1>
+
+            <Link to='/app'>Go to the app</Link>
         </div>
     );
 }
