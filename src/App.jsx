d643797a76202252730ce7ef4800da490c22b64d
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-05-05 12:10:48 +0300

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes , Route, Navigate } from "react-router-dom";
 
 import Product from "./pages/product";
 import Pricing from "./pages/Pricing";
@@ -11,6 +11,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -42,11 +43,11 @@ function App(){
                  <Route path="pricing" element={<Pricing />} />
                  <Route path="login" element={<Login />} />
                  <Route path="app" element={<AppLayout />}>
-                     <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
+                     <Route index element={<Navigate replace to="cities"/>} />
                      <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
                      <Route path="cities/:id" element={<City />} />
                      <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
-                     <Route path="form" element={<p>Form</p>} />
+                     <Route path="form" element={<Form />} />
                  </Route>
                  <Route path="*" element={<PageNotFound />} />
             </Routes>
