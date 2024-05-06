81bf23db71798ea3513f750b2a209162c5533d16
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-05-06 12:34:51 +0300

    #

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { useEffect, useState } from 'react';
 import { useCities } from '../Contexts/CitiesContext';
 
 function Map(){
@@ -11,12 +11,16 @@ function Map(){
     const [mapPosition , setMapPosition]= useState([40 , 0]);
     const[searchParams , setSearchParams] = useSearchParams();
 
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
+    const mapLat = searchParams.get("lat");
+    const mapLng = searchParams.get("lng");
+
+    useEffect(function(){
+        if(mapLat && mapLng) setMapPosition([mapLat , mapLng]);
+    },[mapLat, mapLng])
 
     return(
         <div className={styles.mapContainer}>
-             <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+             <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.map}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -27,9 +31,17 @@ function Map(){
                         <span>{city.cityName}</span>
                     </Popup>
                 </Marker>))}
+
+                <ChangeCenter position={mapPosition} />
             </MapContainer>    
         </div>
     )
+}
+
+function ChangeCenter({position}) {
+    const map = useMap();
+    map.setView(position);
+    return null;
 }
 
 export default Map;
