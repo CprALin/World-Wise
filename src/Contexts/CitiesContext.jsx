77fed3a40f0b952c8fb6667c9eb81a5e7d3e9fd0
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-05-12 13:10:54 +0300

    #

--- src/Contexts/CitiesContext.jsx
+++ src/Contexts/CitiesContext.jsx
@@ -12,6 +12,7 @@ function CitiesProvider({children}) {
    useEffect(function () {
          async function fetchCities() {
           try{
+            setIsLoading(true);
             const res = await fetch(`${BASE_URL}/cities`);
             const data = await res.json();
             setCities(data);
@@ -27,6 +28,7 @@ function CitiesProvider({children}) {
 
    async function getCity(id) {
         try{
+         setIsLoading(true);
           const res = await fetch(`${BASE_URL}/cities/${id}`);
           const data = await res.json();
           setCurrentCity(data);
@@ -35,7 +37,27 @@ function CitiesProvider({children}) {
         }finally{
            setIsLoading(false);
         }
-}
+   }
+
+   async function createCity(newCity) {
+      try{
+         setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities` , {
+            method : "POST",
+            body : JSON.stringify(newCity),
+            headers : {
+               "Content-Type" : "application/json"
+            }
+        });
+        const data = await res.json();
+
+        setCities(cities => [...cities , data])
+      }catch(err){
+         alert("There was an error loading data...");      
+      }finally{
+         setIsLoading(false);
+      }
+   }
 
    return <CitiesContext.Provider value={{cities , isLoading , currentCity , getCity}}>{children}</CitiesContext.Provider>
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,20 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from './Map.module.css'
 import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 import { useCities } from '../Contexts/CitiesContext';
 import { useGeolocation } from '../hooks/useGeolocation';
 import Button from './Button';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 function Map(){
     const {cities} = useCities();
 
     const [mapPosition , setMapPosition]= useState([40 , 0]);
-    const[searchParams , setSearchParams] = useSearchParams();
     const { isLoading : isLoadingPosition , position : geolocationPosition , getPosition } = useGeolocation();
-
-    const mapLat = searchParams.get("lat");
-    const mapLng = searchParams.get("lng");
-
+ 
+    const [mapLat , mapLng] = useUrlPosition();
+ 
     useEffect(function(){
         if(mapLat && mapLng) setMapPosition([mapLat , mapLng]);
     },[mapLat, mapLng])
